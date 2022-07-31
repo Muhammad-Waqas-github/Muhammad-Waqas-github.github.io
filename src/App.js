b90bba3f8ee70dc35e4ccbe28f93d121c76b89f4
Muhammad Waqas <55668440+Muhammad-Waqas-github@users.noreply.github.com>
2022-07-30 22:15:37 -0400

    Add files via upload
    first commit_copied files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+       
+        <a
+          className="Linkedin-link"
+          href="https://ca.linkedin.com/in/muhammad-waqas-canada"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
